704fff9cb08caf6804a29689d7d211ed34a9c4c7
Chima-dan <chimaamaechi39@gmail.com>
2026-04-09 19:49:36 +0100

    firstcommit

---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const mongoose = require('mongoose');
+
+const app = require('./app');
+
+const { loadPlanetsData } = require('./models/planets.model');
+
+const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = 'mongodb+srv://nasa-api:xwOdJ7OtZ6S8QkpY@nasacluster.wwpkfz8.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster'
+
+const server = http.createServer(app);
+
+mongoose.connection.once('open', () =>{
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function startServer() {
+    await mongoose.connect(MONGO_URL);
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer();
